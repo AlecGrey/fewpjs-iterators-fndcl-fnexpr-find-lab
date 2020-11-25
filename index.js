33034721c01b28291cc6b7fe39b82c2666b3c5d9
AlecGrey <alecgrey.coding@gmail.com>
2020-11-25 13:41:14 -0800

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,18 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+const superbowlWin = (arrayOfObjects) => {
+  // object: {year: "2018", result: "L"}
+  const result = arrayOfObjects.find(object => object.result === "W")
+  
+  return !!result ? result.year : undefined
+}
+
+const record = [
+  {year: "2018", result: "L"},
+  {year: "2017", result: "W"},
+  {year: "2016", result: "N/A"}
+]
+
+console.log(superbowlWin(record))
